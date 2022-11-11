2c6ecca5bbfbcb74dfd611d5e92f7557820dc6c3
cesar.carrillo <cesar.carrillo.is@unipolidgo.edu.mx>
2022-11-10 19:46:18 -0600

    integracion de vue y creacion de componente Books

---
+++ frontend/src/components/Book/ListBook.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div class="container">
+        <div class="row">
+            <div class="col text-left">
+                <h2>Listado de libros</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default{
+
+}
+
+</script>
+
+<style lang="css" scoped>
+</style>
